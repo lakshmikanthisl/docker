03335283aa76df72f7d532915a1fe18e3427434c
lakshmikanthisl <67961725+lakshmikanthisl@users.noreply.github.com>
2024-11-05 15:14:02 +0000

    Adding Webpage Demo files

---
+++ nginx-alpine/sript.sh
@@ -0,0 +1,5 @@
+sudo docker run -d -p 80:80 lakshmikanthisl/webdemo:1.0.0
+sudo docker run -d -p 81:80 lakshmikanthisl/webdemo:2.0.0
+sudo docker run -d -p 82:80 lakshmikanthisl/webdemo:3.0.0
+sudo docker run -d -p 83:80 lakshmikanthisl/webdemo:4.0.0
+sudo docker run -d -p 84:80 lakshmikanthisl/webdemo:5.0.0
